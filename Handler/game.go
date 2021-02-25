4ee17ca4049a542f1976e0146ef06e05978dbf21
xingmegshuo <xms.chnb@gmail.com>
2021-02-25 15:12:52 +0800

    update

--- Handler/game.go
+++ Handler/game.go
@@ -125,10 +125,10 @@ func Init(ws *websocket.Conn, room Room) string {
 func RoomUser(room Room) {
 	str := "{'status':'room','mes':'房间成员信息','data':["
 	for l, item := range room.User {
-		if l == len(room.User)-1 {
-			str = str + "{'user':'" + item.OpenID + "','ready:'" + item.Ready +"','onLine':'"+ item.Status+"'},"
+		if l == len(room.User)-1 && l !=0{
+			str = str + "{'user':'" + item.OpenID + "','ready':'" + item.Ready +"','onLine':'"+ item.Status+"'},"
 		} else {
-			str = str + "{'user':'" + item.OpenID + "','ready:'" + item.Ready +"','onLine':'"+ item.Status+"'}"
+			str = str + "{'user':'" + item.OpenID + "','ready':'" + item.Ready +"','onLine':'"+ item.Status+"'}"
 		}
 	
 	}

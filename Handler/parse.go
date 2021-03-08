60a14b80f578dff4e0af5917ba22086f2529bc7d
xingmegshuo <xms.chnb@gmail.com>
2021-03-08 12:50:22 +0800

    update

--- Handler/parse.go
+++ Handler/parse.go
@@ -148,15 +148,18 @@ func RemoveRoom() {
 			ro = DeleteUser(ro, l)
 		}
 		log.Println("还有几个人", len(ro.User))
-		if len(ro.User) > 1{
+		if len(ro.User) > 1 {
 			RoomUser(ro)
 			// str := "{'status':'system','mes':'系统消息','data':{'message':'" + "房间公告:" + openID + "退出房间'}}"
 			// str = strings.Replace(str, "'", "\"", -1)
 			// ServerRoom(ro, str)
+			PlayRoom[i] = ro
 		} else {
 			delete(PlayRoom, i)
 		}
-		PlayRoom[i] = ro
+		if len(client_palyer) == 0 {
+			delte(PlayRoom, i)
+		}
 	}
 }
 

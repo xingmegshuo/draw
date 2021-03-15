f5117f3a22db3f3abbc76572a06a4c616a8234ca
xingmegshuo <xms.chnb@gmail.com>
2021-03-15 19:56:28 +0800

    update

--- Handler/game.go
+++ Handler/game.go
@@ -149,6 +149,7 @@ func Init(ws *websocket.Conn, room Room) string {
 		room.User = append(room.User, player)
 	}
 	room.People = room.People - 1
+	log.Println(room.People)
 	if room.People == 5 {
 		room.Owner = player.OpenID
 	}
